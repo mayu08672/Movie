4aafe16ed358010059b4f694d9a323518c5a5dbd
mayu08672 <mayumayu08261083@gmail.com>
2026-01-26 13:55:29 +0900

    いらないものけした

--- MovieProject/MovieProject/urls.py
+++ MovieProject/MovieProject/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('tv/<int:tv_id>/', views.tv_detail, name='tv_detail'),
      path("api/tmdb/search/", views.tmdb_search),
     path("api/tmdb/discover/", views.tmdb_discover),
-    path('view_questions/', views.view_questions, name='view_questions'),
+    
     path('person/<int:person_id>/', views.person_detail, name='person_detail'),
     path("api/movie/<int:id>/", views.tmdb_movie_detail),
     path("api/tv/<int:id>/", views.tmdb_tv_detail),
@@ -40,11 +40,6 @@ urlpatterns = [
      # サブスク一覧ページ
     path("subscriptions/", views.my_subscriptions, name="my_subscriptions"),
     path('subscriptions/add/', views.my_subscriptions, name='add_subscription'),
-    path('inside_room/', views.inside_room, name='inside_room'),
-    path('view_folder/<int:folder_id>/', views.view_folder, name='view_folder'),
-    path('start_random_quiz/', views.start_random_quiz, name='start_random_quiz'),
-    path('quiz_result/', views.quiz_result, name='quiz_result'),
-    path('leave_room/', views.leave_room, name='leave_room'),
     path('create_account/', views.create_account, name='create_account'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
