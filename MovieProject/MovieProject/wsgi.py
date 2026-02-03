82e059b3f5367877b5ca041173cfc843313de92a
mayu08672 <mayumayu08261083@gmail.com>
2026-02-03 13:46:17 +0900

    wsgi.pyの重複コードを削除

--- MovieProject/MovieProject/wsgi.py
+++ MovieProject/MovieProject/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieProject.settings')
 
 application = get_wsgi_application()
+
+# MovieProject/wsgi.py
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieProject.settings')
+
+application = get_wsgi_application()
+
