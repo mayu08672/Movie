348bb0c00e5092a466f7932ff5e34a3007d8ea25
mayu08672 <mayumayu08261083@gmail.com>
2026-02-07 20:04:18 +0900

    Fix: add __init__.py so Movie.views is a package

---
+++ MovieProject/Movie/views/__init__.py
@@ -0,0 +1,8 @@
+from .auth_views import *
+from .latest_movie_views import *
+from .login_views import *
+from .create_account_views import *
+from .movie_detail_views import *
+from .person_detail_views import *
+from .tmdb_views import * 
+# from .subscription_views import *
